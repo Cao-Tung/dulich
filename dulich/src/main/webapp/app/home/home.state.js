327ee22a26d25ac1b1746734e1dd2083999ea8f7
Cao-Tung <caotungjs@gmail.com>
2017-05-17 15:21:37 +0700

    update page search

---
+++ dulich/src/main/webapp/app/home/home.state.js
@@ -0,0 +1,35 @@
+(function() {
+    'use strict';
+
+    angular
+        .module('dulichApp')
+        .config(stateConfig);
+
+    stateConfig.$inject = ['$stateProvider'];
+
+    function stateConfig($stateProvider) {
+        $stateProvider.state('home', {
+            parent: 'app',
+            url: '/',
+            data: {
+                authorities: []
+            },
+            views: {
+                'content@': {
+                    templateUrl: 'app/home/home.html',
+                    controller: 'HomeController',
+                    controllerAs: 'vm'
+                }
+            },
+            resolve: {
+                // region: ['$stateParams','Region',function($stateParams,Region){
+                //     return Region.get({id : $stateParams.id}).$promise;
+                // }], // lấy ra các region theo id
+                mainTranslatePartialLoader: ['$translate', '$translatePartialLoader', function ($translate,$translatePartialLoader) {
+                    $translatePartialLoader.addPart('home');
+                    return $translate.refresh();
+                }]
+            }
+        });
+    }
+})();
